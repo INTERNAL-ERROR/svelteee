c27489e098f41faf6340564dbc6a3cf45ef183f6
Coder's Digest <94619537+INTERNAL-ERROR@users.noreply.github.com>
2022-04-01 22:26:09 +0000

    MongoDB test for serverless netlify!

--- app/src/routes/api/signup.js
+++ app/src/routes/api/signup.js
@@ -1,10 +1,30 @@
 import {createClient} from "@astrajs/collections"
 import jwt from "jsonwebtoken"
+import { MongoClient, ServerApiVersion } from "mongodb"
+
 
 export async function post ({request}) {
     let body = await request.json();
     console.log("asdf")
     console.log(body.hello);
+   
+        const uri = "mongodb+srv://ethano:SenorGatoSpeakseo941248169@survey.4offd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+        MongoClient.connect(uri, function(err, db) {
+          if (err) throw err;
+          var dbo = db.db("mydb");
+          var myobj = {hello:"world"}
+            
+            dbo.collection("customers").insertOne(myobj, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                
+              });
+            dbo.collection("customers").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+        });
     
     return {status: 418, body: {
         wow: "hi!"
